71573ae163dca595f621985fbdc707c7ece91a66
lab_services_student <lab_services_student@labVMH8OX>
2024-03-20 15:55:30 +0200

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class UserController : Controller
+    {
+        public Table_1 usrtbl = new Table_1();
+
+        [HttpPost]
+        public ActionResult About(Table_1 Users)
+        {
+            var result = usrtbl.insert_User(Users);
+            return RedirectToAction("Home","About");
+        }
+        [HttpGet]
+        public ActionResult About()
+        {
+            return View();
+        }      
+            public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Table_1.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+
+
+namespace WebApplication1.Models
+{
+    public class Table_1 : Controller
+    {
+        public static string con_string = "Server=tcp:cldv-server-sql.database.windows.net,1433;Initial Catalog=cldv-DB;Persist Security Info=False;User ID=stephane-kib;Password=CTMMonecole1@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
+            public static SqlConnection con = new SqlConnection(con_string);
+
+        public String Name { get; set; }
+        public String SurName { get; set; }
+        public String Email { get; set; }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public int insert_User(Table_1 m)
+        {
+            string sql = "INSERT INTO Table_1 (userName, userSurname, userEmail) VALUES(@Name, @SurName,@Email)";
+                SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.Parameters.AddWithValue("@Name", m.Name);
+            cmd.Parameters.AddWithValue("@SurnName", m.SurName);
+            cmd.Parameters.AddWithValue("@Email", m.Email);
+            con.Open();
+            int rowsAffected = cmd.ExecuteNonQuery();   
+            con.Close();
+            return rowsAffected;
+        }
+    }
+}
